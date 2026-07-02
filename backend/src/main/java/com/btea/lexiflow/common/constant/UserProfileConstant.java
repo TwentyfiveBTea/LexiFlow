b93dfe45cc98b6e30c795e85a763fe03698348c9
TwentyfiveBTea <1248102041@qq.com>
2026-07-03 01:53:08 +0800

    feat: 添加用户认证数据传输对象

---
+++ backend/src/main/java/com/btea/lexiflow/common/constant/UserProfileConstant.java
@@ -0,0 +1,14 @@
+package com.btea.lexiflow.common.constant;
+
+/**
+ * @Author: TwentyfiveBTea
+ * @Date: 2026/7/2 23:38
+ * @Description: 用户信息常量
+ */
+public class UserProfileConstant {
+
+    /**
+     * 默认头像
+     */
+    public static final String DEFAULT_AVATAR = "https://lexi-flow.oss-cn-guangzhou.aliyuncs.com/avatar/dufault.jpg";
+}
