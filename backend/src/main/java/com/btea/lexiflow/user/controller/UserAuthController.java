f995b4658ce14a751fe7a91e5e0d1be45cc89f61
TwentyfiveBTea <1248102041@qq.com>
2026-07-03 14:46:38 +0800

    feat: 调整认证接口版本路径

--- backend/src/main/java/com/btea/lexiflow/user/controller/UserAuthController.java
+++ backend/src/main/java/com/btea/lexiflow/user/controller/UserAuthController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/lexiflow/auth")
+@RequestMapping("/api/v1/auth")
 public class UserAuthController {
 
     private final UserAuthService userAuthService;
